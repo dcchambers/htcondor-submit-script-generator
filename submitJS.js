820af54bd8f24aa607f2d073331c6a065dd615fe
Dakota Chambers <dakotachambers@gmail.com>
2015-05-27 12:33:36 -0500

    Finally have some html form generation using the chtcrequirements.txt config file. Changed config file format.

--- submitJS.js
+++ submitJS.js
@@ -1,5 +1,9 @@
 //submit JS file
 
+
+/* This function generates the Condor Submit Script based on the HTML
+ * that the user fills out. It is called with a submit button on the page.
+ */
 function generateScript(){
     document.getElementById("outputHeader").style.display="block";
     document.getElementById("output").style.display="block";
@@ -16,7 +20,11 @@ function generateScript(){
     window.scrollTo(0,document.body.scrollHeight);
 }
 
-
+/* The readConfigFile() function checks to make sure the browser supports reading
+ * from a file, and then reads the config file (currently chtcrequirements.txt)
+ * that will be used to create the HTML form.
+ *
+ */
 function readConfigFile(){
     // Check for the various File API support.
     if (window.File && window.FileReader && window.FileList && window.Blob) {
@@ -35,21 +43,28 @@ function readConfigFile(){
     }
         
     var arrLines = strRawContents.split("\n");
-    fileDisplayArea.innerText +="File " + oFrame.src + " has " + arrLines.length + " lines."+"\n\n";
     for (var i = 0; i < arrLines.length; i++) {
         var curLine = arrLines[i];
-        //alert("Line #" + (i + 1) + " is: '" + curLine + "'");
-        //fileDisplayArea.innerText += curLine+"\n";
-        generateForm(curLine);
+        if (curLine[0] != "#"){ //ignore comments.
+	    generateForm(curLine);
+	}
+	
     }
     
 }
 
+/* generateForm(String) uses the config file to create the HTML form
+ * that the user is presented with.
+ *
+ */
 function generateForm(textLine){
   fileDisplayArea.innerText += textLine+"\n"; //DEBUG
+
+  var splitLine = textLine.split(","); //split the line up by commas into an array.
+
   document.getElementById('form').innerHTML += 
-    "<input type=\"checkbox\" name=\"ArchINTEL\" value=\"INTEL\">32 Bit<br>"; //formDisplayArea.innerHTML wasn't working right.
-  
+      "<input type=\"checkbox\" name=\"ARCH\" value=\"INTEL\">"+splitLine[2]+"<br>"; //formDisplayArea.innerHTML wasn't working right.
+
   
 }
 
